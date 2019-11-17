cda40330094de5f414a0f175490af116780e1ab4
Italo Sayan <italo.sayan@glovoapp.com>
2019-11-17 20:42:05 +0100

    define request function and formatting function.

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from datetime import datetime
-
+import pandas as pd
 
 URL_CRIME_60_DAYS = "https://maps.cityofrochester.gov" \
                     "/arcgis/rest/services/RPD" \
@@ -23,13 +23,54 @@ OUTPUT_FORMAT = "&outSR=4326&f=json"
 
 REQUEST_URL = URL_CRIME_60_DAYS + FIELDS_STRING + ",".join(FIELDS) + OUTPUT_FORMAT
 
-rochester_crime_data = requests.get(REQUEST_URL)
-rochester_crime_data = json.loads(rochester_crime_data.content)
 
-now = datetime.now()
+def crime_request_to_dict(url):
+    """
+    :param url: Format to access the rpdny API
+    https://data-rpdny.opendata.arcgis.com/datasets/rpd-part-i-crime-60-days/geoservice
 
-employ_data = open('./S3_Responses/.csv', 'w')
+    :return: Python dictionary of 60 days of crime data.
+    """
+    rochester_crime_data = requests.get(url)
+    rochester_crime_data = json.loads(rochester_crime_data.content)
 
-rochester_crime_data['features'][0]['geometry']['x']
-rochester_crime_data['features'][0]['geometry']['y']
-rochester_crime_data['features'][0]['attributes']
+    return rochester_crime_data
+
+
+def dict_to_data_frame(dict_data):
+    """
+    :param dict_data: Request response in dictionary format
+    :return: pandas data frame of one crime per row.
+    """
+    empty_geometry = {'x': None,
+                      'y': None}
+
+    empty_attr = {'OBJECTID': None,
+                  'OccurredFrom_Timestamp': None,
+                  'OccurredThrough_Timestamp': None,
+                  'Reported_Timestamp': None,
+                  'Statute_Text': None,
+                  'Statute_Description': None,
+                  'Weapon_Description': None,
+                  'Larceny_Type': None,
+                  'Location_Type': None}
+
+    attributes = [i['attributes'] if 'attributes' in i else empty_attr for i in dict_data['features']]
+    geometry = [i['geometry'] if 'geometry' in i else empty_geometry for i in dict_data['features']]
+
+    attributes = pd.DataFrame(attributes)
+    geometry = pd.DataFrame(geometry)
+
+    data_frame_of_crimes = pd.concat([attributes, geometry], axis=1, sort=False)
+
+    return data_frame_of_crimes
+
+
+if __name__ == '__main__':
+    """
+    This file requests the rochester crime data to the RPD-NY API. 
+    Then, formats the return to a consumable format.
+    """
+    roc_crimes = crime_request_to_dict(REQUEST_URL)
+    data_frame_roc_crimes = dict_to_data_frame(roc_crimes)
+    now = str(datetime.now())
